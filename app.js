25d8dc8e95a0c8959f8ee9fe4621ef87f20a0a2d
Ayanadiya <ayanadiya17@gmail.com>
2024-10-16 15:18:50 +0530

    routers and filters

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
 const http = require('http');
 const express=require('express');
+const adminroutes=require('./routers/admin');
+const shoproutes=require('./routers/shop');
 const bodyParser = require('body-parser');
 const app=express();
 app.use(bodyParser.urlencoded({extended:false}));
-app.use('/add-product',(req,res,next)=>{
-    console.log('In another middleware');
-    res.send('<form action="/product" method="POST"><input type="text" name="title" ><input type="text" name="size" ><button type="submit">Add Product</button></form>');
-})
-app.post('/product',(req,res,next)=>{
-        console.log(req.body);
-        res.redirect('/');
-})
-app.use('/',(req,res,next)=>{
-    console.log('In another middleware');
-    res.send('<h1>Hello from Express</h1>');
+app.use('/admin',adminroutes);
+app.use(shoproutes);
+app.use((req,res,next)=>{
+     res.status(404).send('<h1>Page not found</h1>')
 })
 app.listen(4000);
